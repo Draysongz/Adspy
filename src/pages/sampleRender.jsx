0d26bfd66498ee8e612842438c499645a9812022
ovpn-dev <ovpn05@gmail.com>
2023-10-29 15:45:18 +0100

    just build ffs

--- src/pages/sampleRender.jsx
+++ src/pages/sampleRender.jsx
@@ -1,19 +1,9 @@
 import AiSidebar from "@/components/AiBoard/SideBar";
-import ChangePassword from "@/pages/profile/changepassword";
-import ManageSubscriptions from "@/pages/profile/manageSubscriptions";
-import ProfileHome from "@/pages/profile/profileHome";
-import ProfileSidebar from "@/pages/profile/ProfileSidebar";
-import { Flex } from "@chakra-ui/react";
-import UserDash from "./admin/userDash";
-import ProfileDash from "./admin/userDash";
 
 export default function SampleRender() {
   return (
     <>
-      {" "}
       <AiSidebar />
-      {/* <ManageSubscriptions /> */}
-      {/*      <ProfileHome />  <ProfileDash /> <UserDash /> <ProfileSidebar /> */}
     </>
   );
 }
